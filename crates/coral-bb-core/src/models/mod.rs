1f267a153dc4a2a37466817c6b21be5d7ecd2287
kbastin <kbastin@gmail.com>
2026-03-27 09:24:08 +0100

    Add subforum.

--- crates/coral-bb-core/src/models/mod.rs
+++ crates/coral-bb-core/src/models/mod.rs
@@ -1,6 +1,7 @@
 pub mod user;
 pub mod thread;
 pub mod message;
+pub mod sub_forum;
 
 pub use user::User;
 pub use user::NewUser;
@@ -10,3 +11,6 @@ pub use thread::NewThread;
 
 pub use message::Message;
 pub use message::NewMessage;
+
+pub use sub_forum::SubForum;
+pub use sub_forum::NewSubForum;
---
+++ crates/coral-bb-core/src/models/sub_forum.rs
@@ -0,0 +1,12 @@
+pub struct SubForum {
+    id: u32,
+    name: String,
+    description: String,
+    parent_forum_id: Option<u32>,
+}
+
+pub struct NewSubForum {
+    name: String,
+    description: String,
+    parent_forum_id: Option<u32>,
+}
