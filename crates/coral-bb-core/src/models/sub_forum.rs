c1d34f0cf9c8f087d0a2b5d7511a450465f71965
kbastin <kbastin@gmail.com>
2026-03-27 09:42:55 +0100

    Update name

--- crates/coral-bb-core/src/models/sub_forum.rs
+++ crates/coral-bb-core/src/models/sub_forum.rs
@@ -2,11 +2,11 @@ pub struct SubForum {
     id: u32,
     name: String,
     description: String,
-    parent_forum_id: Option<u32>,
+    parent_sub_forum_id: Option<u32>,
 }
 
 pub struct NewSubForum {
     name: String,
     description: String,
-    parent_forum_id: Option<u32>,
+    parent_sub_forum_id: Option<u32>,
 }
